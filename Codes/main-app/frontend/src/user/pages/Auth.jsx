95ff2ba8da75e52b960292139b2d4f847574e31e
Tejas Angadi <tejasangadi456@gmail.com>
2024-02-21 07:15:34 +0530

    Handled the login part using useForm Hook

--- Codes/main-app/frontend/src/user/pages/Auth.jsx
+++ Codes/main-app/frontend/src/user/pages/Auth.jsx
@@ -1,13 +1,27 @@
 import { useForm } from "react-hook-form";
 
+import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
 
-const Auth = () => {
-  const { register, handleSubmit } = useForm();
+const Auth = (props) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    // defaultValues: { email: "test@test.com", password: "test123@" },
+  });
+
+  // console.log("Watching: ", watch());
+  const password = watch("password");
+  const email = watch("email");
+
   return (
     <div>
-      Login Required
       <div className="place-form">
+        <h2 className="center">Login Required</h2>
+        <hr />
         <form
           className={`form-control`}
           onSubmit={handleSubmit((data) => {
@@ -18,19 +32,40 @@ const Auth = () => {
             type="email"
             id="email"
             className="inp"
-            {...register("email", { required: true })}
+            {...register("email", { required: "This required" })}
           />
+          <p>{errors.email?.message}</p>
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
             className="inp"
-            {...register("password", { required: true, minLength: 5 })}
+            {...register("password", {
+              required: true,
+              minLength: {
+                value: 5,
+                message: "Password should be more than 5 characters",
+              },
+            })}
           />
-          <p style={{ fontSize: "14px", color: "red" }}>
-            Minimum length of the password should be 5
-          </p>
-          <button>Login</button>
+          <p>{errors.password?.message}</p>
+          {email?.includes("@") && password?.length > 5 ? (
+            <button
+              className={`button button--${props.size || "default"} ${
+                props.inverse && "button--inverse"
+              } ${props.danger && "button--danger"}`}>
+              Login
+            </button>
+          ) : (
+            <button
+              className={`button button--${props.size || "default"} ${
+                props.inverse && "button--inverse"
+              } ${props.danger && "button--danger"}`}
+              disabled>
+              Login
+            </button>
+          )}
+
           {/* <Button to=" ">Login</Button> */}
         </form>
       </div>
